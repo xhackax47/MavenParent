07c2206ce75f21faaeb0a57f0d1f3c35df85a522
Samy Chaabi <xhackax47@gmail.com>
2018-09-20 17:20:12 +0200

    update Junit

---
+++ AppliWeb/src/test/java/MaClasseTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MaClasseTest {
+
+@Test
+  public void testCalculer() throws Exception {
+
+    assertEquals(2,MaClasse.calculer(1,1));
+  }
+}
